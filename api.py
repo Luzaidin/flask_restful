3c02e32d2d14db475270dbb7161b7e98c42fbbc7
Luis Belo <luzaidinbelo@gmail.com>
2022-05-17 21:54:27 -0300

    Add: novas rotas

--- api.py
+++ api.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask_restful import Api
-from resource.User.user import User
+from resource.User.user import ListUser, CreateUser, DeleteUser
 
 app = Flask(__name__)
 api = Api(app)
 
 # Routes
-api.add_resource(User, '/user/<int:id>')
+api.add_resource(ListUser, '/list/user/<int:id>')
+api.add_resource(DeleteUser, '/delete/user/<int:id>')
+api.add_resource(CreateUser, '/create/user')
+DeleteUser
 
 if __name__ == '__main__':
     app.run(debug=True)
